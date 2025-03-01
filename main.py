266b7e4be4cdc2b3340c9b7c589b8aa6e570f055
Areeba1213 <164592607+Areeba1213@users.noreply.github.com>
2025-03-01 06:37:43 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+# DATA TYPE IN PYTHONE
+# STRING (STR)
+name = "Python"
+print(type(name))  # Output: <class 'str'>
+
+# INTEGER (int)
+x = 10   # Integer
+y = -25  # Negative integer
+print(type(x))  # Output: <class 'int'>
+
+# LIST (list)
+fruits = ["Apple", "Banana", "Cherry"]
+print(type(fruits))  # Output: <class 'list'>
+
+# TUPLE (tuple)
+coordinates = (10.5, 20.3)
+print(type(coordinates))  # Output: <class 'tuple'>
+
+# DICTIONARY(dict)
+student = {"name": "Ali", "age": 20, "grade": "A"}
+print(type(student))  # Output: <class 'dict'>
+
+# BOOLEAN (bool)(true or false)
+is_python_fun = True
+is_sky_pink = False
+print(type(is_python_fun))  # Output: <class 'bool'>
+
+# SET (set)
+colors = {"red", "green", "blue"}
+print(type(colors))  # Output: <class 'set'>
+# FROZEN SET (frozen set)
+immutable_set = frozenset(["a", "b", "c"])
+print(type(immutable_set))  # Output: <class 'frozenset'>
+
